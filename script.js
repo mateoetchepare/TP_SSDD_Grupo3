85175c070e8a0fb94a42535aad5dfb5b442d8e3d
mostri28 <mateoetchepare@gmail.com>
2023-10-09 20:20:06 -0300

    terminando por hoy

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-const selectBtn = document.querySelector(".select-btn"),
-      items = document.querySelectorAll(".item");
-selectBtn.addEventListener("click", () => {
-    selectBtn.classList.toggle("open");
+// Obtener todos los botones de selección y elementos de ítem
+const selectBtns = document.querySelectorAll(".select-btn");
+const items = document.querySelectorAll(".item");
+
+// Agregar un evento de clic a cada botón de selección
+selectBtns.forEach((selectBtn, index) => {
+    selectBtn.addEventListener("click", () => {
+        // Obtener los elementos relacionados para el picker específico
+        const itemsForPicker = items[index];
+        selectBtn.classList.toggle("open");
+        itemsForPicker.classList.toggle("open");
+    });
 });
-items.forEach(item => {
+
+// Agregar un evento de clic a cada elemento de ítem
+items.forEach((item, index) => {
     item.addEventListener("click", () => {
         item.classList.toggle("checked");
-        let checked = document.querySelectorAll(".checked"),
-            btnText = document.querySelector(".btn-text");
-            if(checked && checked.length > 0){
-                btnText.innerText = `${checked.length} Pisos Permitidos`;
-            }else{
-                btnText.innerText = "0 Pisos Permitidos";
-            }
+        // Obtener los elementos relacionados para el picker específico
+        const picker = item.closest(".picker");
+        const btnText = picker.querySelector(".btn-text");
+        const checked = picker.querySelectorAll(".checked");
+        
+        if (checked && checked.length > 0) {
+            btnText.innerText = `${checked.length} Pisos Permitidos`;
+        } else {
+            btnText.innerText = "0 Pisos Permitidos";
+        }
     });
-})
+});
