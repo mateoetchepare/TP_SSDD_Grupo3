29e79ac9e640550919ca5cdf1429924a1c1c6afa
Joaquin Acuña <joaquinacunadestree@gmail.com>
2023-10-24 22:37:46 -0300

    metodo post alta visitante

--- GestionVisitantes/index.js
+++ GestionVisitantes/index.js
@@ -6,8 +6,34 @@ const server = http.createServer((req,res)=>{
 
     console.log(`URL: ${url} - METHOD: ${method}`);
 
-    
+    //falta verificar ruteos segun url
 
+    // metodo POST para alta de un visitante
+    if (method === 'POST'){
+        let body = ''
+        req.setEncoding('utf8')
+
+        req.on('data', (chunk) =>{
+            body += chunk
+        })
+
+        req.on('end', () =>{
+            console.log(body)
+
+            try {
+                const jsonData = JSON.parse(body)
+
+                console.log('JSON data: ', jsonData)
+
+            } catch(error){
+                res.statusCode = 400
+                res.end('Error analizando JSON')
+            }
+        })
+
+    }
+
+    res.end('Solicitud recibida exitosamente')
 })
 
 server.listen(3501)//puerto de gestion visitantes
