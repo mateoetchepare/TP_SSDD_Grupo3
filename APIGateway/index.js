be9554cba0922bb6e2478efd2046098c48396e53
Tomi Bedini <bedinitomas@gmail.com>
2023-10-25 22:33:45 -0300

    a

--- APIGateway/index.js
+++ APIGateway/index.js
@@ -1,11 +1,19 @@
 const http = require('http');
 const { bodyParser } = require('./bodyParser');
+//const url = require('url');
 
 const server = http.createServer((req,res) => {
 
     const { url , method } = req;
     console.log(`URL: ${url} - METHOD: ${method}`);
 
+    //const parsedUrl = url.parse(req.url, true); se refiere al paquete que esta comentado arriba el url, no a la variable url
+
+    let param = url.split("/");
+
+    if(url.startsWith("")){
+
+    }
 
 });
 
