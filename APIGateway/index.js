420ca21629b463670b90c1b68a49e1fade1fc198
Tomi Bedini <bedinitomas@gmail.com>
2023-10-30 00:41:33 -0300

    sigo con apigateway

--- APIGateway/index.js
+++ APIGateway/index.js
@@ -2,6 +2,7 @@ const http = require('http');
 const { bodyParser } = require('./bodyParser');
 const url = require('url');
 const puertoAscensores = 3502;
+const puertoVisitantes = 3501;
 
 const server = http.createServer((req,res) => {
     
@@ -80,9 +81,104 @@ const server = http.createServer((req,res) => {
 
             //case "PUT":
         }
+    }
+
+    if(url.startsWith("/visitantes")){
+        switch(method){
+            case "GET":
+                http.get("http://localhost:" + puertoVisitantes + url,
+                (respuesta) =>{
+                    let data = "";
+                    respuesta.on("data", (chunk) => {
+                        data += chunk;
+                    });
+                    respuesta.on("end", () => {
+                        res.writeHead(respuesta.statusCode, {
+                            "Content-Type": "application/json",
+                        });
+                        data = JSON.parse(data);
+                        res.write(JSON.stringify(data));
+                        res.end();
+                    });
+                })
+                .on("error", (err) => {
+                    console.log(err.message);
+                })
+
+                break;
+            
+            case "POST":
+                bodyParser(req)
+                    .then(() => {
+                        const options = {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                        };
+                    
+                        const request = http.request("http://localhost:" + puertoVisitantes + url, 
+                        options,
+                        function(response){
+                            let body = "";
+
+                            response.on("data", (chunk) => {
+                                body += chunk;
+                            });
+
+                            response.on("end", () => {
+                                res.writehead(response.statusCode,{
+                                    "Content-Type": "application/json",
+                                });
+                                body = JSON.parse(body);
+                                res.write(JSON.stringify(body));
+                                res.end();
+                            });
+
+                            response.on("close", () => {
+                                console.log("Connection closed");
+                            })
+
+                        });
+
+                        if(param[1] == ""){ //hay que definir la interfaz
+
+                        }
+                
+                
+                    })
+            
+            case "DELETE":
+                bodyParser(req) //OJO ACA, VER BIEN
+                    .then(() => {
+                        payload = JSON.stringify({
+                            userID: req.body.userID,
+                        });
+                        const options = {
+                            method: "DELETE",
+                            headers:{
+                                "Content-Type": "application/json",
+                                "Content-Length": Buffer.byteLength(req.body),
+                            },
+                        };
+
+                        const request = http.request("http://localhost:" + puertoVisitantes + url,
+                        options,
+                        function(response){
+                            let body = "";
+
+                            response.on("data", (chunk) => {
+                                
+                            })
 
 
+                        }
+                        )
 
+                    })
+
+            //case "PUT":
+        }
     }
 
 });
