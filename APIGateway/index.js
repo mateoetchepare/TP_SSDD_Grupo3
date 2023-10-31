562a914a3abdafa543fe01d6d089b5f5d271e22d
Tomi Bedini <bedinitomas@gmail.com>
2023-10-30 23:03:10 -0300

    Api gateway mas completa

--- APIGateway/index.js
+++ APIGateway/index.js
@@ -13,77 +13,7 @@ const server = http.createServer((req,res) => {
 
     let param = url.split("/"); //chequear que este bien spliteado
 
-    if(url.startsWith("/ascensores")){
-        switch(method){
-            case "GET":
-                http.get("http://localhost:" + puertoAscensores + url,
-                (respuesta) =>{
-                    let data = "";
-                    respuesta.on("data", (chunk) => {
-                        data += chunk;
-                    });
-                    respuesta.on("end", () => {
-                        res.writeHead(respuesta.statusCode, {
-                            "Content-Type": "application/json",
-                        });
-                        data = JSON.parse(data);
-                        res.write(JSON.stringify(data));
-                        res.end();
-                    });
-                })
-                .on("error", (err) => {
-                    console.log(err.message);
-                })
-
-                break;
-            
-            case "POST":
-                bodyParser(req)
-                    .then(() => {
-                        const options = {
-                            method: "POST",
-                            headers: {
-                                "Content-Type": "application/json",
-                            },
-                        };
-                    
-                        const request = http.request("http://localhost:" + puertoAscensores + url, 
-                        options,
-                        function(response){
-                            let body = "";
-
-                            response.on("data", (chunk) => {
-                                body += chunk;
-                            });
-
-                            response.on("end", () => {
-                                res.writehead(response.statusCode,{
-                                    "Content-Type": "application/json",
-                                });
-                                body = JSON.parse(body);
-                                res.write(JSON.stringify(body));
-                                res.end();
-                            });
-
-                            response.on("close", () => {
-                                console.log("Connection closed");
-                            })
-
-                        });
-
-                        if(param[1] == ""){ //hay que definir la interfaz
-
-                        }
-                
-                
-                    })
-            
-            //case "DELETE":
-
-            //case "PUT":
-        }
-    }
-
+    // VISITANTES
     if(url.startsWith("/api/visitantes")){
         switch(method){
             case "GET":
@@ -144,7 +74,7 @@ const server = http.createServer((req,res) => {
                             });
 
                             response.on("close", () => {
-                                console.log("Connection closed.");
+                                console.log("Connection closed Visitantes.");
                             })
 
                         });
@@ -153,8 +83,6 @@ const server = http.createServer((req,res) => {
                         request.end();
                 
                     }).catch((error) => console.error(error));
-
-                    
 
                 break;
             
@@ -186,29 +114,104 @@ const server = http.createServer((req,res) => {
                     });
 
                     response.on("close", () => {
-                        console.log("Connection closed.")
+                        console.log("Connection closed Visitantes.")
                     });
                 })
                 
-                //request.write();
                 request.end();
                 break;
 
             case "PUT": //a chequear
-                bodyParser(req) //OJO ACA, VER BIEN
-                    .then(() => {
-                        /*payload = JSON.stringify({
-                            userID: req.body.userID,
-                        });*/
-                        const options = {
-                            method: "PUT",
-                            headers:{
-                                "Content-Type": "application/json",
-                                //"Content-Length": Buffer.byteLength(req.body),
-                            },
-                        };
+                bodyParser(req)
+                .then(() => {
 
-                        const request = http.request("http://localhost:" + puertoVisitantes + url,
+                    const bodyData = req.body;
+
+                    const options = {
+                        method: "PUT",
+                        headers: {
+                            "Content-Type": "application/json",
+                        }
+                    };
+            
+                    const request = http.request("http://localhost:" + puertoVisitantes + url, 
+                    options,
+                    function(response){
+                        let body = "";
+
+                        response.on("data", (chunk) => {
+                            body += chunk;
+                        });
+
+                        response.on("end", () => {
+                            res.writeHead(response.statusCode,{
+                                "Content-Type": "application/json",
+                            });
+                            console.log('el body es: ',body)
+                            //body = JSON.parse(body);
+                            //res.write(JSON.stringify(body));
+                            res.write(body)
+                            res.end();
+                        });
+
+                        response.on("close", () => {
+                            console.log("Connection closed Visitantes.");
+                        })
+
+                    });
+
+                    request.write(JSON.stringify(bodyData));
+                    request.end();
+        
+                }).catch((error) => console.error(error));
+
+            break;   
+
+        }
+    }
+
+
+    //ASCENSORES
+    if(url.startsWith("/ascensores")){
+        switch(method){
+            case "GET":
+                http.get("http://localhost:" + puertoAscensores + url,
+                (respuesta) =>{
+                    let data = "";
+                    respuesta.on("data", (chunk) => {
+                        data += chunk;
+                    });
+                    respuesta.on("end", () => {
+                        res.writeHead(respuesta.statusCode, {
+                            "Content-Type": "application/json",
+                        });
+                        console.log('la data es: ',data)
+                        //data = JSON.parse(data);
+                        //res.write(JSON.stringify(data));
+                        res.write(data)
+                        res.end();
+                    });
+                })
+                .on("error", (err) => {
+                    console.log(err.message);
+                })
+
+                break;
+            
+            case "POST":
+                bodyParser(req)
+                    .then(() => {
+
+                        const bodyData = req.body;
+
+                        const options = {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                            }
+                        };
+                    
+                        const request = http.request("http://localhost:" + puertoAscensores + url, 
                         options,
                         function(response){
                             let body = "";
@@ -221,26 +224,194 @@ const server = http.createServer((req,res) => {
                                 res.writeHead(response.statusCode,{
                                     "Content-Type": "application/json",
                                 });
-                                body = JSON.parse(body);
-                                res.write(JSON.stringify(body));
+                                console.log('el body es: ',body)
+                                //body = JSON.parse(body);
+                                //res.write(JSON.stringify(body));
+                                res.write(body)
                                 res.end();
                             });
 
                             response.on("close", () => {
-                                console.log("Connection closed.")
-                            });
+                                console.log("Connection closed Ascensores.");
+                            })
 
-                        })
+                        });
 
-                        //request.write(payload);
+                        request.write(JSON.stringify(bodyData));
                         request.end();
+                
                     }).catch((error) => console.error(error));
 
                 break;
+            
+            case "DELETE":
+                const options = {
+                    method: "DELETE",
+                    headers:{
+                        "Content-Type": "application/json"//,
+                       // "Content-Length": Buffer.byteLength(req.body),
+                    },
+                };
+
+                const request = http.request("http://localhost:" + puertoAscensores + url,
+                options,
+                function(response){
+                    let data = "";
+                    response.on("data", (chunk) => {
+                        data += chunk;
+                    });
+
+                    response.on("end", () => {
+                        res.writeHead(response.statusCode,{
+                            "Content-Type": "application/json",
+                        });
+                        //data = JSON.parse(data);
+                        //res.write(JSON.stringify(data));
+                        res.write(data)
+                        res.end();
+                    });
+
+                    response.on("close", () => {
+                        console.log("Connection closed Ascensores.")
+                    });
+                })
+                
+                request.end();
+                break;
+
+            case "PUT":
+                bodyParser(req)
+                .then(() => {
+
+                    const bodyData = req.body;
+
+                    const options = {
+                        method: "PUT",
+                        headers: {
+                            "Content-Type": "application/json",
+                        }
+                    };
+            
+                    const request = http.request("http://localhost:" + puertoAscensores + url, 
+                    options,
+                    function(response){
+                        let body = "";
+
+                        response.on("data", (chunk) => {
+                            body += chunk;
+                        });
+
+                        response.on("end", () => {
+                            res.writeHead(response.statusCode,{
+                                "Content-Type": "application/json",
+                            });
+                            console.log('el body es: ',body)
+                            //body = JSON.parse(body);
+                            //res.write(JSON.stringify(body));
+                            res.write(body)
+                            res.end();
+                        });
+
+                        response.on("close", () => {
+                            console.log("Connection closed Ascensores.");
+                        })
+
+                    });
+
+                    request.write(JSON.stringify(bodyData));
+                    request.end();
+        
+                }).catch((error) => console.error(error));
+
+            break;   
 
         }
     }
 
+
+    //PERMISOS
+    if(url.startsWith("/api/permisos")){
+        switch(method){
+            case "GET":
+                http.get("http://localhost:" + puertoPermisos + url,
+                (respuesta) =>{
+                    let data = "";
+                    respuesta.on("data", (chunk) => {
+                        data += chunk;
+                    });
+                    respuesta.on("end", () => {
+                        res.writeHead(respuesta.statusCode, {
+                            "Content-Type": "application/json",
+                        });
+                        console.log('la data es: ',data)
+                        //data = JSON.parse(data);
+                        //res.write(JSON.stringify(data));
+                        res.write(data)
+                        res.end();
+                    });
+                })
+                .on("error", (err) => {
+                    console.log(err.message);
+                })
+
+                break;
+
+
+            case "PUT":
+                bodyParser(req)
+                .then(() => {
+
+                    const bodyData = req.body;
+
+                    const options = {
+                        method: "PUT",
+                        headers: {
+                            "Content-Type": "application/json",
+                        }
+                    };
+            
+                    const request = http.request("http://localhost:" + puertoPermisos + url, 
+                    options,
+                    function(response){
+                        let body = "";
+
+                        response.on("data", (chunk) => {
+                            body += chunk;
+                        });
+
+                        response.on("end", () => {
+                            res.writeHead(response.statusCode,{
+                                "Content-Type": "application/json",
+                            });
+                            console.log('el body es: ',body)
+                            //body = JSON.parse(body);
+                            //res.write(JSON.stringify(body));
+                            res.write(body)
+                            res.end();
+                        });
+
+                        response.on("close", () => {
+                            console.log("Connection closed Permisos.");
+                        })
+
+                    });
+
+                    request.write(JSON.stringify(bodyData));
+                    request.end();
+        
+                }).catch((error) => console.error(error));
+
+            break;  
+
+
+        }
+
+    }
+
+
 });
+
+
+
 
 server.listen(3500); // puerto del APIRest
