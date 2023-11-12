bd6e8d9cea0bdb23f68e243e90e16809c590a73c
sosasanti <santiagososa@mdp.edu.ar>
2023-11-12 16:36:57 -0300

    ruteo cambiado (no anda todavia)

---
+++ auth.js
@@ -0,0 +1,63 @@
+function Hash(email) {
+    var hash = 0;
+    if (email.length == 0) return hash;
+    for (i = 0; i < email.length; i++) {
+        char = email.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Lo convierte a un entero de 32 bit 
+    }
+    return (hash<0)?hash*-1:hash;
+}
+
+auth0
+  .createAuth0Client({
+    //responseType: "code",
+    //audience: "https://dev-pn7zgl7ckp8stzea.us.auth0.com/api/v2/",
+    domain: "dev-h74gvie66akma5zh.us.auth0.com",
+    clientId: "HeNnWrtoPJVYK7vFbQnVpcf2LPzOzCu",
+    authorizationParams: {
+      redirect_uri: window.location.origin + '#/visitantes',
+    },
+  })
+  .then(async (auth0Client) => {
+    // Assumes a button with id "login" in the DOM
+    const loginButton = document.getElementById("login");
+
+    loginButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      console.log(window.location.origin);
+      auth0Client.loginWithRedirect().then(token => {console.log(token)
+        });
+    });
+
+    if (location.search.includes("state=") &&
+      (location.search.includes("code=") ||
+        location.search.includes("error="))) {
+      auth0Client.handleRedirectCallback().then((token) => {
+        auth0Client.getIdTokenClaims().then(sessionData => {
+          console.log(token)
+          console.log(sessionData)
+          if (sessionData) {
+            window.sessionStorage.setItem('token', sessionData.__raw)
+            window.sessionStorage.setItem('email', sessionData.email)
+            window.sessionStorage.setItem('userId', Hash(sessionData.email))
+          }
+          window.history.replaceState({}, document.title, "/");
+          window.location.href = "#/visitantes";
+         
+        });
+      });
+    }
+
+    // Assumes a button with id "logout" in the DOM
+
+    const logoutButton = document.getElementById("logout");
+
+    logoutButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      auth0Client.logout();
+    });
+
+    const isAuthenticated = await auth0Client.isAuthenticated();
+    const userProfile = await auth0Client.getUser();
+  });
